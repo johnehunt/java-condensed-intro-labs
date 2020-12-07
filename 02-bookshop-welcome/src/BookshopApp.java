1cff320f690932a3d775a3b25e64b3ff3f0d8058
John Hunt <john.hunt10@gmail.com>
2020-12-07 10:12:51 +0000

    Added a simple hello world Java app

---
+++ 02-bookshop-welcome/src/BookshopApp.java
@@ -0,0 +1,6 @@
+public class BookshopApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Java Bookshop");
+        System.out.println("============================");
+    }
+}
